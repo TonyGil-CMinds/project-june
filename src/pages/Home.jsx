568da29870c8950d96f1b93f3c8ff69416dbba49
TonyGil-CMinds <tony@cminds.co>
2026-04-30 10:24:09 -0600

    hitos btn url

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -602,7 +602,7 @@ export default function Home({ appReady }) {
             <span className="word">el</span>{' '}
             <span className="word">Caribe</span>
           </p>
-          <a href="#" className="btn-glass-outline css-glass" id="mission-cta">
+          <a href="https://www.naturatech.org/hechoconsuecia" className="btn-glass-outline css-glass" id="mission-cta">
             <svg width="14" height="14" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2.5"><line x1="12" y1="5" x2="12" y2="19"/><line x1="5" y1="12" x2="19" y2="12"/></svg>
             Ver Hitos
           </a>
