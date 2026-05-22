571f4999e02d9a4ec2a8623d7904e50a39010ede
TonyGil-CMinds <tony@cminds.co>
2026-05-22 12:15:35 -0600

    text studio444

--- src/views/Studio.jsx
+++ src/views/Studio.jsx
@@ -821,7 +821,7 @@ export default function Studio() {
           <div className="studio-main-text">
             <h3>
               Un portafolio vivo de innovación para la
-              <span>(biodiversidad</span>
+              <span>biodiversidad</span>
             </h3>
             <p>Financiamos, co-desarrollamos y validamos soluciones regenerativas junto a organizaciones territoriales, emprendedores y aliados estratégicos para acelerar nuevas formas de conservar, regenerar y generar valor desde la biodiversidad.</p>
             <p className="studio-main-stat">1.6M de invertidos en la naturaleza</p>
