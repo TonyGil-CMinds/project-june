56de5b499d70401d43aa0a77f6749b67b8dcd9fd
TonyGil-CMinds <tony@cminds.co>
2026-05-25 18:40:10 -0600

    chabfes

--- next.config.mjs
+++ next.config.mjs
@@ -5,14 +5,3 @@ const nextConfig = {
 
 export default nextConfig;
 
-module.exports = {
-  async redirects() {
-    return [
-      {
-        source: '/500',
-        destination: 'https://500.naturatech.org',
-        permanent: true,
-      },
-    ];
-  },
-};
